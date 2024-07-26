859a940fd033f21692bd6f1b258f2531ab0515bd
murakami <murakami@kokochie.jp>
2024-07-26 09:53:46 +0900

    laravel 11ではapi.phpないから追加

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
+
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('api')->group(function () {
-    // APIルートの定義
-});
-// Route::get('/my-posts', [PostController::class, 'myPosts'])->name('posts.myPosts');
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
